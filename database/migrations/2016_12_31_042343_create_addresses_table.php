9c0a5f90079ec58dd359df813b6399d5ba6b9bac
webdevmatics <webdevmatics@gmail.com>
2016-12-31 19:34:18 +0545

    Fixes and shipping store

---
+++ database/migrations/2016_12_31_042343_create_addresses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('addressline');
+            $table->string('city');
+            $table->string('state');
+            $table->integer('zip');
+            $table->string('country');
+            $table->integer('phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
