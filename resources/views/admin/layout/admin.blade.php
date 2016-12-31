cd57cc4ba92e95ca99a158a5f273f37f8509d1ed
webdevmatics <webdevmatics@gmail.com>
2016-12-31 07:39:30 +0545

    Frontend validation parsley

--- resources/views/admin/layout/admin.blade.php
+++ resources/views/admin/layout/admin.blade.php
@@ -5,15 +5,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" media="screen" href="https://netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{asset('css/admin.css')}}">
+    <link rel="stylesheet" href="{{asset('css/parsley.css')}}">
 </head>
 <body>
 @include('admin.layout.includes.header')
 <div class="page-content">
-    @if(Session::has('message'))
-        <div class="alert alert-info">
-            <p>{{ Session::get('message') }}</p>
-        </div>
-    @endif
+        @if(Session::has('message'))
+            <div class="alert alert-info">
+                <p>{{ Session::get('message') }}</p>
+            </div>
+        @endif
+            @if (count($errors) > 0)
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
 
     <div class="row">
         @include('admin.layout.includes.sidenav')
@@ -21,7 +31,7 @@
             <div class="row text-center">
                 <div class="col-md-10 col-md-offset-1">
                     <div class="content-box-large">
-                            @yield('content')
+                        @yield('content')
                     </div>
                 </div>
             </div>
@@ -31,6 +41,7 @@
 </div><!--/Page Content-->
 
 <script src="https://code.jquery.com/jquery.js"></script>
+<script src="{{asset('js/parsley.min.js')}}"></script>
 <script src="https://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 <script>
     $(document).ready(function () {
--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -6,11 +6,11 @@
 
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            {!! Form::open(['route' => 'product.store', 'method' => 'POST', 'files' => true]) !!}
+            {!! Form::open(['route' => 'product.store', 'method' => 'POST', 'files' => true, 'data-parsley-validate'=>'']) !!}
 
             <div class="form-group">
                 {{ Form::label('name', 'Name') }}
-                {{ Form::text('name', null, array('class' => 'form-control')) }}
+                {{ Form::text('name', null, array('class' => 'form-control','required'=>'','minlength'=>'5')) }}
             </div>
 
             <div class="form-group">
