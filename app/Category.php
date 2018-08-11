cde18d40b8a708822029b6fc277c276eb3ff23d0
subhendu <bhattasubhendu@gmail.com>
2018-08-11 22:48:44 +0545

    changes:

--- app/Category.php
+++ app/Category.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable=['name'];
+    protected $fillable=['name','slug'];
     protected $table='product_categories';
 
     public function products()
